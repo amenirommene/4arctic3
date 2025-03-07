e04af44fff9a882d2327e3e707c2636f363e829a
amenir88 <ameni.rommene@esprit.tn>
2025-03-07 09:37:53 +0100

    model apartment

--- src/app/core/models/apartment.ts
+++ src/app/core/models/apartment.ts
@@ -1,4 +1,13 @@
-export class Apartment
-{
-  id! : number;
+import { Residence } from "./residence";
+
+export class Apartment {
+    id!: number;
+    appartNum!: number;
+    floorNum!: number;
+    surface!: number;
+    terrace!: string;
+    surfaceTerrace!: number;
+    category!: string;
+    description!: string;
+    residence!: Residence;
 }
